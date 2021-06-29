9fbeb62a91c1550a4d846ef06207bcb34fe56081
esaterkec <46487118+esaterkec@users.noreply.github.com>
2021-06-29 21:04:38 +0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace TestConnectionString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection conn = new SqlConnection("Server=localhost;Database=SQLShackDemo;User Id=SuperHero;Password=1pass;"))
+            {
+                conn.Open();
+                Console.WriteLine("Connection just opened");
+                System.Threading.Thread.Sleep(100000);
+                conn.Close();
+            }
+        }
+    }
+}
